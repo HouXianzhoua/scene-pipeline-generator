c4d2d6ad0a1903eaa9e3557892ef6eace521756c
houxianzhou <2683180773@qq.com>
2026-05-22 17:30:54 +0800

    Remove legacy evals compatibility package

--- evals/scene_pipeline/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Compatibility wrapper for the former ``evals.scene_pipeline`` package."""
-
-from __future__ import annotations
-
-from scene_pipeline import *  # noqa: F401,F403
-from scene_pipeline import __path__ as __path__
